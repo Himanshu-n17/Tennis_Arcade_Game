2e9f14a1bf98c160ad63e214eacd6922012bdce8
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-06 10:16:29 +0530

    Arcade Game

--- g.js
+++ g.js
@@ -19,10 +19,10 @@ var showingWinScreen = false;
 var winnerText = ""; //Name of winner
 var gameStarted = false; // Flag to check if game has started
 
-// Sound effects
-var hitPaddleSound = new Audio("./Sounds/pong_hit.wav");
-var wallBounceSound = new Audio("./Sounds/tennis_ball.wav");
-var scoreSound = new Audio("./Sounds/score_beep.mp3");
+// Load sound effects
+var hitPaddleSound = new Audio("hit_paddle.wav");
+var wallBounceSound = new Audio("wall_bounce.wav");
+var scoreSound = new Audio("score.wav");
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
@@ -44,30 +44,26 @@ window.onload = function () {
 
 function initializeGame() {
   var framesPerSecond = 30;
-  //To constantly changing the position
   setInterval(callBoth, 1000 / framesPerSecond);
 
-  //To Calculate the mouse Position
   canvas.addEventListener("mousemove", function (evt) {
     var MousePos = calculateMousePos(evt);
     paddle1Y = MousePos.y - paddle_height / 2;
   });
 
-  // to stop the game when any player wins the game
   canvas.addEventListener("click", function () {
     if (showingWinScreen) {
       showingWinScreen = false;
       Player1Score = 0;
       Player2Score = 0;
       winnerText = "";
-      paddle_height = initial_paddle_height; // Reset paddle size
-      ballSpeedX = 10; //Reset Speed of ball
+      paddle_height = initial_paddle_height;
+      ballSpeedX = 10;
       ballSpeedY = 4;
     }
   });
 }
 
-//function to run when ball does not hit the paddle
 function ballReset() {
   if (Player1Score >= WINNING_SCORE || Player2Score >= WINNING_SCORE) {
     showingWinScreen = true;
@@ -75,12 +71,12 @@ function ballReset() {
       Player1Score >= WINNING_SCORE ? "Player 1 Wins!" : "YOU LOSE!!!";
     return;
   }
-  ballSpeedX = -ballSpeedX; //to change the direction if not hit
-  ballX = canvas.width / 2; //to start the ball from center of screen
+  scoreSound.play(); // Play score sound
+  ballSpeedX = -ballSpeedX;
+  ballX = canvas.width / 2;
   ballY = canvas.height / 2;
 }
 
-// Function to track mouse movement for paddle control
 function calculateMousePos(evt) {
   var rect = canvas.getBoundingClientRect();
   var root = document.documentElement;
@@ -89,40 +85,33 @@ function calculateMousePos(evt) {
   return { x: MouseX, y: MouseY };
 }
 
-//Function to move the paddle according to the ball (i.e AI)
 function ComputerMovement() {
   var paddle2YCenter = paddle2Y + paddle_height / 2;
   var speed = 10 + Math.random() * 2;
-  //if paddle position is small than ball posn it will (i.e ball is down than paddle) it will move the paddle down
   if (paddle2YCenter < ballY - 35) {
     paddle2Y += speed;
-  }
-  //if paddle position is larger than ball posn it will (i.e ball is upper than paddle) it will move the paddle upward
-  else if (paddle2YCenter > ballY + 35) {
+  } else if (paddle2YCenter > ballY + 35) {
     paddle2Y -= speed;
   }
 }
 
 function moveEverything() {
-  //If final score reached game stops
-  if (showingWinScreen) {
-    return;
-  }
+  if (showingWinScreen) return;
 
   ComputerMovement();
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
   if (ballX > canvas.width - paddle_width - 10) {
-    //if ball hits paddle area it will bounce else reset the ball.
     if (ballY > paddle2Y && ballY < paddle2Y + paddle_height) {
+      hitPaddleSound.play(); // Play paddle hit sound
       let hitPosition =
         (ballY - (paddle2Y + paddle_height / 2)) / (paddle_height / 2);
       let angle = (hitPosition * Math.PI) / 3;
       let speed = Math.sqrt(ballSpeedX * ballSpeedX + ballSpeedY * ballSpeedY);
       ballSpeedX = -speed * Math.cos(angle);
       ballSpeedY = speed * Math.sin(angle);
-      ballSpeedX *= 1.04; // Increase speed slightly after each paddle hit
+      ballSpeedX *= 1.04;
       ballSpeedY *= 1.04;
     } else {
       Player1Score++;
@@ -130,23 +119,24 @@ function moveEverything() {
     }
   }
   if (ballX < paddle_width + 10) {
-    //if ball hits paddle area it will bounce else reset the ball.
     if (ballY > paddle1Y && ballY < paddle1Y + paddle_height) {
+      hitPaddleSound.play(); // Play paddle hit sound
       let hitPosition =
         (ballY - (paddle1Y + paddle_height / 2)) / (paddle_height / 2);
       let angle = (hitPosition * Math.PI) / 3;
       let speed = Math.sqrt(ballSpeedX * ballSpeedX + ballSpeedY * ballSpeedY);
       ballSpeedX = speed * Math.cos(angle);
       ballSpeedY = speed * Math.sin(angle);
-      ballSpeedX *= 1.05; // Increase speed slightly after each paddle hit
+      ballSpeedX *= 1.05;
       ballSpeedY *= 1.05;
-      paddle_height = Math.max(50, paddle_height - 2); // Decrease paddle height over time but not below 50
+      paddle_height = Math.max(50, paddle_height - 2);
     } else {
       Player2Score++;
       ballReset();
     }
   }
   if (ballY > canvas.height - 10 || ballY < 10) {
+    wallBounceSound.play(); // Play wall bounce sound
     ballSpeedY = -ballSpeedY;
   }
 }
@@ -156,11 +146,9 @@ function drawNet() {
     colorRect(canvas.width / 2 - 1, i, 2, 20, "white");
   }
 }
-function drawEverything() {
-  //The screen
-  colorRect(0, 0, canvas.width, canvas.height, "black");
 
-  //Finish Game
+function drawEverything() {
+  colorRect(0, 0, canvas.width, canvas.height, "black");
   if (showingWinScreen) {
     canvasContext.fillStyle = "white";
     canvasContext.font = "30px Arial";
@@ -178,11 +166,7 @@ function drawEverything() {
     return;
   }
   drawNet();
-
-  // Left Side Paddle
   colorRect(10, paddle1Y, paddle_width, paddle_height, "white", "paddle");
-
-  // Right Side Paddle
   colorRect(
     canvas.width - paddle_width - 10,
     paddle2Y,
@@ -191,31 +175,15 @@ function drawEverything() {
     "white",
     "paddle"
   );
-
-  // Ball
   colorCircle(ballX, ballY, 5, "white", "ball");
-
-  // Scores
   canvasContext.fillStyle = "white";
   canvasContext.font = "30px Arial";
   canvasContext.fillText(Player1Score, canvas.width / 4, 50);
   canvasContext.fillText(Player2Score, (canvas.width * 3) / 4, 50);
 }
 
-function colorCircle(CenterX, CenterY, radius, drawColor, className = "") {
-  canvasContext.fillStyle = drawColor;
-  canvasContext.beginPath();
-  canvasContext.arc(CenterX, CenterY, radius, 0, Math.PI * 2, true);
-  canvasContext.fill();
-}
-
-function colorRect(leftX, topY, width, height, drawColor, className = "") {
-  canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect(leftX, topY, width, height);
-}
-
 function callBoth() {
-  if (!gameStarted) return; // Don't run if game hasn't started
+  if (!gameStarted) return;
   moveEverything();
   drawEverything();
 }
